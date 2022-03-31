25695f1b0e5cd61f82a29069a90a4d70408279f4
FunkyZombie <slpinter.hertz@gmail.com>
2022-04-01 02:41:10 +0700

    lesson 3

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+const burger = document?.querySelector('[data-burger]');
+const nav = document?.querySelector('[data-nav]');
+const slider = document?.querySelector('[slider-box]');
+
+burger?.addEventListener('click', () => {
+    burger?.classList.toggle('burger--active');
+    nav?.classList.toggle('nav--visible');
+    slider?.classList.toggle('slider--shadow');
+});
